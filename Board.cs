d833ce6661bff63846ac613c1f2cd5bf0b82579c
stoianpp <stoianpp2013@gmail.com>
2014-07-11 01:59:00 +0300

    draw figures methods added to the board

--- Board.cs
+++ Board.cs
@@ -7,12 +7,14 @@
         private const int TableSize = 8;
         private const int CellWidth = 6;
         private const int CellHeight = 4;
+        private const int XStart = 40;
+        private const int YStart = 20;
         private static Board instance;
 
         private Board()
         {
             DrawBoard();
-
+            DrawPlayers();
         }
 
         public static Board Instance
@@ -28,7 +30,6 @@
         }
         public static void DrawBoard()
         {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.BackgroundColor = ConsoleColor.DarkGray;
             for (int i = 0; i < TableSize; i++)
             {
@@ -36,7 +37,7 @@
                 {
                     for (int k = 0; k < CellHeight; k++)
                     {
-                        Console.SetCursorPosition(40 + i * CellWidth, 20 + g * CellHeight + k);
+                        Console.SetCursorPosition(XStart + i * CellWidth, YStart + g * CellHeight + k);
                         if ((g % 2 + i % 2) == 0 || (g % 2 - i % 2) == 0)
                         {
                             Console.BackgroundColor = ConsoleColor.Gray;
@@ -54,7 +55,33 @@
         }
         public static void DrawPlayers()
         {
-
+            DrawPawn(0, 0);
+            DrawPawn(2, 0);
+            DrawPawn(4, 0);
+            DrawPawn(6, 0);
+            DrawKing(3, 7);
+        }
+        public static void DrawPawn(int x,int y) 
+        {
+            string[] pawn = {"  /\\  "," /||\\ "," \\||/ "," /||\\ "};
+            for (int i = 0; i < pawn.Length; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.SetCursorPosition(XStart+x*CellWidth,YStart+y*CellHeight+i);
+                Console.WriteLine(pawn[i]);
+            }
+        }
+        public static void DrawKing(int x, int y)
+        {
+            string[] king = { "/\\/\\/\\", "\\    /", " |  | ", "/____\\" };
+            for (int i = 0; i < king.Length; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(XStart + x * CellWidth, YStart + y * CellHeight + i);
+                Console.WriteLine(king[i]);
+            }
         }
     }
 }
