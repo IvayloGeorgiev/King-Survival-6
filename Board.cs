2ab9d293ddbc1f5214e18643336e398face11999
stoianpp <stoianpp2013@gmail.com>
2014-07-10 22:42:40 +0300

    some changes on the board

--- Board.cs
+++ Board.cs
@@ -5,11 +5,14 @@
     public class Board
     {
         private const int TableSize = 8;
+        private const int CellWidth = 6;
+        private const int CellHeight = 4;
         private static Board instance;
 
         private Board()
         {
             DrawBoard();
+
         }
 
         public static Board Instance
@@ -31,18 +34,18 @@
             {
                 for (int g = 0; g < TableSize; g++)
                 {
-                    for (int k = 0; k < 2; k++)
+                    for (int k = 0; k < CellHeight; k++)
                     {
-                        Console.SetCursorPosition(40 + i * 3, 20 + g * 2 + k);
+                        Console.SetCursorPosition(40 + i * CellWidth, 20 + g * CellHeight + k);
                         if ((g % 2 + i % 2) == 0 || (g % 2 - i % 2) == 0)
                         {
                             Console.BackgroundColor = ConsoleColor.Gray;
-                            Console.Write("   ");
+                            Console.Write(new String(' ', CellWidth));
                             Console.BackgroundColor = ConsoleColor.DarkGray;
                         }
                         else
                         {
-                            Console.Write("   ");
+                            Console.Write(new String(' ',CellWidth));
                         }
                     }
                 }
