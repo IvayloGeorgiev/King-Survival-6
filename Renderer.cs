7ea0087e8433c3e086c4c507883031c389d5c876
stoianpp <stoianpp2013@gmail.com>
2014-07-22 00:31:38 +0300

    renderer update

---
+++ Renderer.cs
@@ -0,0 +1,30 @@
+﻿namespace KingSurvivalGame
+{
+    using System;
+
+    public class Renderer
+    {
+        public void DrawFigure(IDrawable figToDraw)
+        {
+            for (int i = 0; i < figToDraw; i++)
+            {
+                int[] boardMeasures = Board.Instance.BoardMeasures;
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.SetCursorPosition(boardMeasures[0] + figToDraw.Position[0] * boardMeasures[1],
+                                          boardMeasures[2] + figToDraw.Position[1] * boardMeasures[3] + i);
+                Console.WriteLine(figToDraw[i]);
+            }
+        }
+        
+        public string GetInputRequest()
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(30, 39);
+            Console.Write("    Enter the next move: ");
+            string playerRequest = Console.ReadLine();
+            return playerRequest;
+        }
+    }
+}
